00fed23534614a5c61cc0c79f8a6879ee700a445
R-Maxime <styleshoot34@gmail.com>
2024-01-28 19:23:39 +0100

    Fix date + Hover on Collapsible__trigger

--- Front/src/pages/Work/E-bou/Statistics.tsx
+++ Front/src/pages/Work/E-bou/Statistics.tsx
@@ -9,9 +9,9 @@ function getDate(date: Date): string {
   const day = date.getDate();
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const year = date.getFullYear();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
 
   return `${day}/${month}/${year} à ${hours}:${minutes}:${seconds}`;
 }
